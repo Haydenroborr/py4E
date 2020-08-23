4923e1fe070c349eb6ec468675e34e99c87eb62e
HaydenRobOrr <HaydenRobOrr@gmail.com>
2020-08-22 22:01:34 -0400

    Refactor: Split print statement into two print statements

--- maxAndMin.py
+++ maxAndMin.py
@@ -2,10 +2,14 @@ max = None
 min = None
 
 
-for number in [4, 5, 7, 16, 3]:
-    if min is None or number < min:
-        min = number
-    if max is None or number > max:
-        max = number
+for number in [7, 2, "bob", 10, 4]:
+    try:
+        if min is None or number < min:
+            min = number
+        if max is None or number > max:
+            max = number
+    except:
+        print("Invalid input")
 
-print("Final:", min, max)
+print("Maximum is", max)
+print("Minimum is", min)
