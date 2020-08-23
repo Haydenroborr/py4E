6dfb83bbbd67b9f1f227a6d991e9cd2497047918
HaydenRobOrr <HaydenRobOrr@gmail.com>
2020-08-22 22:18:42 -0400

    Refactor: shorten Try/Except section

--- Done/maxAndMinInput.py
+++ Done/maxAndMinInput.py
@@ -4,20 +4,22 @@ min = None
 # Enter 7, 2, bob, 10, 4
 while True:
     number = input("Enter a number: ")
+
+    if number == "done":
+        break
     try:
-        if number == "done":
-            break
         number = float(number)
-        if min is None:
-            min = float(number)
-        if max is None:
-            max = float(number)
-        if float(number) < min:
-            min = number
-        if float(number) > max:
-            max = number
     except:
         print("Invalid input")
+        continue
+    if min is None:
+        min = float(number)
+    if max is None:
+        max = float(number)
+    if float(number) < min:
+        min = number
+    if float(number) > max:
+        max = number
 
 print("Maximum is", int(max))
 print("Minimum is", int(min))
