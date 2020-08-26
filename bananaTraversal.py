fcb592bd824da8df44e8dd8ce3d88bad4498f6d9
HaydenRobOrr <HaydenRobOrr@gmail.com>
2020-08-25 20:27:14 -0400

    Feat: Add new letters counter

--- bananaTraversal.py
+++ bananaTraversal.py
@@ -8,3 +8,15 @@ while counter < len(fruit):
     counter = counter + 1
     index = index - 1
 print(fruit)
+
+def count(word, letter):
+    counter = 0
+    for letters in word:
+        if letters == str(letter):
+            counter = counter + 1
+
+    print('There is', counter, letter, 'in', word + ".")
+
+count(fruit, "b")
+count(fruit, "a")
+count(fruit, "n")
