02e43d721145914028dab772b079a70035783015
HaydenRobOrr <HaydenRobOrr@gmail.com>
2020-08-25 20:11:05 -0400

    First commit

---
+++ bananaTraversal.py
@@ -0,0 +1,10 @@
+counter = 0
+fruit = "banana"
+index = -1
+
+while counter < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    counter = counter + 1
+    index = index - 1
+print(fruit)
