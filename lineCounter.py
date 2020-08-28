2705bc92af4ffce84f9a4ddf26dba844203d93b3
HaydenRobOrr <HaydenRobOrr@gmail.com>
2020-08-27 20:16:00 -0400

    Feat: Add easter egg

--- lineCounter.py
+++ lineCounter.py
@@ -1,10 +1,15 @@
 fileName = input('Enter the file name: ')
+if fileName == "na na boo boo":
+    print("NA NA BOO BOO TO YOU - You have been punk'd!")
+    exit()
 try:
     fileName = open(fileName)
 except:
     print('File cannot be opened:', fileName)
     exit()
+
 count = 0
 for line in fileName:
-    count = count + 1
+    if line.startswith('Subject:'):
+        count = count + 1
 print('There were', count, 'lines in the file.')
